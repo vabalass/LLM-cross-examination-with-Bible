f9af3351fc78ace9f00021510c454f90b8a33698
vabalass <vabalas.balys@gmail.com>
2025-10-27 16:46:48 +0200

    created function to get llm answer

--- cross-examination.py
+++ cross-examination.py
@@ -2,6 +2,21 @@ from pathlib import Path
 from litellm import completion
 import os
 
+# returns a bible question with 4 choice answers (a, b, c, d)
+def get_bible_question_from_llm(model = "gemini/gemini-2.5-flash", bible_text = ""):
+    if model is not None and bible_text != "":
+        message_content = "Sukurk vieną klausimą su keturiais a, b, c ir d atsakymų variantais " \
+        "iš mano duoto Biblijos skyriaus. Tik vienas atsakymus turi būti teisingas. " + bible_text
+        message = [{ "content": message_content,"role": "user"}]
+        response = completion(model=model, messages=message)
+
+        if response.choices:
+            return response.choices[0].message["content"]
+        else:
+            print("Error: No choices found in the response.")
+    else:
+        raise ValueError("Model and bible_text must be provided")
+
 def read_and_save_API_keys(api_keys_path):
     if api_keys_path := Path(api_keys_path):
         with api_keys_path.open("r", encoding="utf-8") as f:
@@ -12,13 +27,10 @@ def read_and_save_API_keys(api_keys_path):
 
 def main():
     read_and_save_API_keys("API_keys.txt")
+
     bible_pr1 = "".join((Path(__file__).parent / "Bible" / "Pr1.txt").read_text(encoding="utf-8"))
 
-    message_content = "Sukurk vieną klausimą su keturiais a, b, c ir d atsakymų variantais " \
-    "iš mano duoto skyriaus. Tik vienas atsakymus turi būti teisingas. " + bible_pr1
-    message = [{ "content": message_content,"role": "user"}]
-    response = completion(model="gemini/gemini-2.5-flash", messages=message)
-    print(response.choices[0].message["content"])
+    print(get_bible_question_from_llm(model="gemini/gemini-2.5-flash", bible_text=bible_pr1))
 
 if __name__ == "__main__":
     main()
